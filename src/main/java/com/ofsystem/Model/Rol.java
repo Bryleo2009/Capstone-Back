25063728ba940c20b6ea17e6ac60838481b55c8d
AlvaroHitz <alvarocuzquen1011@gmail.com>
2023-03-30 13:33:00 -0500

    Correccion Rol

--- src/main/java/com/ofsystem/Model/Rol.java
+++ src/main/java/com/ofsystem/Model/Rol.java
@@ -1,13 +1,13 @@
 package com.ofsystem.Model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -16,23 +16,10 @@ import lombok.Data;
 public class Rol {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public int id_rol;
-	@Column(name = "nombre_tp", nullable = false, length = 20)
+	public int idRol;
+	@Column(name = "nombreTp", nullable = false, length = 20)
     @Size(min = 3, message = "Minimo 3 caracteres")
-	public String nombre_rol;
-	public int getId_rol() {
-		return id_rol;
-	}
-	public void setId_rol(int id_rol) {
-		this.id_rol = id_rol;
-	}
-	public String getNombre_rol() {
-		return nombre_rol;
-	}
-	public void setNombre_rol(String nombre_rol) {
-		this.nombre_rol = nombre_rol;
-	}
-
+	public String nombreRol;
 	
 	
 }
