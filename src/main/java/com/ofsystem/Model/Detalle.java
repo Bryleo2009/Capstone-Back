8040fffc711bc0f306ee80d0b9e2206bee9d4b14
AlvaroHitz <alvarocuzquen1011@gmail.com>
2023-03-30 13:26:54 -0500

    Correcion Detalle

--- src/main/java/com/ofsystem/Model/Detalle.java
+++ src/main/java/com/ofsystem/Model/Detalle.java
@@ -1,88 +1,38 @@
 package com.ofsystem.Model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table (name = "detalle_comprobante")
+@Table (name = "detalleComprobante")
 @Data
 public class Detalle {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public int id_dcomp;
-	@Column(name = "cantProduct_detalle", nullable = false)
-	public int cantProduct_detalle; //10
-	@Column(name = "precioUni_detalle", nullable = false)
-	public double precioUni_detalle; //0.20
-	@Column(name = "precioTotal_detalle", nullable = false)
-	public double precioTotal_detalle; //2.00
-	@Column(name = "producto_detalle", nullable = false)
-	public String producto_detalle;
-	@Column(name = "id_product", nullable = true)
-	public int id_product;
+	public int idDcomp;
+	@Column(name = "cantProductDetalle", nullable = false)
+	public int cantProductDetalle; //10
+	@Column(name = "precioUniDetalle", nullable = false)
+	public double precioUniDetalle; //0.20
+	@Column(name = "precioTotalDetalle", nullable = false)
+	public double precioTotalDetalle; //2.00
+	@Column(name = "productoDetalle", nullable = false)
+	public String productoDetalle;
+	@Column(name = "idProduct", nullable = true)
+	public int idProduct;
 	public String imagen;
 	@ManyToOne
-    @JoinColumn(name="id_comp", referencedColumnName = "id_comp")
-	public Comprobante id_comp;
+    @JoinColumn(name="idComp", referencedColumnName = "idComp")
+	public Comprobante idComp;
 	
-
-	
-	public String getImagen() {
-		return imagen;
-	}
-	public void setImagen(String imagen) {
-		this.imagen = imagen;
-	}
-	public int getId_product() {
-		return id_product;
-	}
-	public void setId_product(int id_product) {
-		this.id_product = id_product;
-	}
-	public int getId_dcomp() {
-		return id_dcomp;
-	}
-	public void setId_dcomp(int id_dcomp) {
-		this.id_dcomp = id_dcomp;
-	}
-	public int getCantProduct_detalle() {
-		return cantProduct_detalle;
-	}
-	public void setCantProduct_detalle(int cantProduct_detalle) {
-		this.cantProduct_detalle = cantProduct_detalle;
-	}
-	public double getPrecioUni_detalle() {
-		return precioUni_detalle;
-	}
-	public void setPrecioUni_detalle(double precioUni_detalle) {
-		this.precioUni_detalle = precioUni_detalle;
-	}
-	public double getPrecioTotal_detalle() {
-		return precioTotal_detalle;
-	}
-	public void setPrecioTotal_detalle(double precioTotal_detalle) {
-		this.precioTotal_detalle = precioTotal_detalle;
-	}
-	public Comprobante getId_comp() {
-		return id_comp;
-	}
-	public void setId_comp(Comprobante id_comp) {
-		this.id_comp = id_comp;
-	}
-	public String getProducto_detalle() {
-		return producto_detalle;
-	}
-	public void setProducto_detalle(String producto_detalle) {
-		this.producto_detalle = producto_detalle;
-	}
 	
 	
 	
