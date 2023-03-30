a00c10a94aee9cafff5edfb1509e019febd5533b
AlvaroHitz <alvarocuzquen1011@gmail.com>
2023-03-30 13:36:30 -0500

    Correccion TipoCompro

--- src/main/java/com/ofsystem/Model/TipoCompro.java
+++ src/main/java/com/ofsystem/Model/TipoCompro.java
@@ -1,13 +1,13 @@
 package com.ofsystem.Model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -16,21 +16,10 @@ import lombok.Data;
 public class TipoCompro {
     @Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-    public int id_tc;
-    @Column(name = "nombre_tc", nullable = false, length = 20)
+    public int idTc;
+    @Column(name = "nombreTc", nullable = false, length = 20)
     @Size(min = 3, message = "Minimo 3 caracteres")
-    public String nombre_tc;
+    public String nombreTc;
 
-	public int getId_tc() {
-		return id_tc;
-	}
-	public void setId_tc(int id_tc) {
-		this.id_tc = id_tc;
-	}
-	public String getNombre_tc() {
-		return nombre_tc;
-	}
-	public void setNombre_tc(String nombre_tc) {
-		this.nombre_tc = nombre_tc;
-	}
+	
 }
