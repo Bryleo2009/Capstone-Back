34309b7fab3a353f1a4d661b15f679f5e27fb0af
AlvaroHitz <alvarocuzquen1011@gmail.com>
2023-03-31 15:31:53 -0500

    flujo tipo comprobante

---
+++ src/main/java/com/ofsystem/Service/Imple/TipoComproServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ofsystem.Service.Imple;
+
+import com.ofsystem.Model.TipoCompro;
+import com.ofsystem.Repo.ITipoComproRepo;
+import com.ofsystem.Service.ITipoComproService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TipoComproServiceImpl extends CRUDServiceImpl<TipoCompro, Integer> implements ITipoComproService{
+
+	@Autowired
+	private ITipoComproRepo repo;
+	
+	@Override
+	protected JpaRepository<TipoCompro, Integer> getRepo() {
+		return repo;
+	}
+
+}
