4296c3b8cc7b53719b5b8ab55740a4a92fba0874
Alvaro Cuzquen <alvarocuzquen1011@gmail.com>
2023-05-29 23:38:43 -0500

    listar_pedidos

--- src/main/java/com/ofsystem/Service/Service/Cliente/IPedidoService.java
+++ src/main/java/com/ofsystem/Service/Service/Cliente/IPedidoService.java
@@ -1,10 +1,16 @@
 package com.ofsystem.Service.Service.Cliente;
 
+import com.ofsystem.Mapper.Filter.SeguimientoPedidoFilter;
 import com.ofsystem.Model.Cliente.Pedido;
 import com.ofsystem.Service.Service.ICRUDService;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface IPedidoService extends ICRUDService<Pedido, Integer> {
     Pedido findByIdComp_Iuc(String iuc);
 
     Pedido findByIdComp_IdComp (String idComp);
+
+    List<SeguimientoPedidoFilter> listarPedido (int idUser);
 }
