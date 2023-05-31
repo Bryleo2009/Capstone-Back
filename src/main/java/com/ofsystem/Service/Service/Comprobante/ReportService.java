481688f687600fbcae4f1dff14089e4dc20d5074
Bryan Andrés Morán Vega <bryleo2009@hotmail.com>
2023-05-31 12:14:05 -0500

    avance

--- src/main/java/com/ofsystem/Service/Service/Comprobante/ReportService.java
+++ src/main/java/com/ofsystem/Service/Service/Comprobante/ReportService.java
@@ -17,23 +17,23 @@ public class ReportService {
     @Autowired
     private IComprobanteService service;
 
-    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
-        List<Comprobante> comprobantes = service.findAll();
-        // cargar datos
-        File file = ResourceUtils.getFile("classpath:comprobante.jrxml");
-        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
-        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(comprobantes);
-        Map<String, Object> parameters = new HashMap<>();
-        parameters.put("CreatedBy", "JULIAN SAC");
-        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
-
-        if(reportFormat.equalsIgnoreCase("html")){
-            JasperExportManager.exportReportToHtmlFile(jasperPrint,"C:\\Users\\Adrian\\Desktop\\Report"+"\\comprobante.html");
-        }
-        if (reportFormat.equalsIgnoreCase("pdf")){
-
-        }
-
-        return "";
-    }
+//    public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+//        List<Comprobante> comprobantes = service.findAll();
+//        // cargar datos
+//        File file = ResourceUtils.getFile("classpath:comprobante.jrxml");
+//        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+//        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(comprobantes);
+//        Map<String, Object> parameters = new HashMap<>();
+//        parameters.put("CreatedBy", "JULIAN SAC");
+//        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+//
+//        if(reportFormat.equalsIgnoreCase("html")){
+//            JasperExportManager.exportReportToHtmlFile(jasperPrint,"C:\\Users\\Adrian\\Desktop\\Report"+"\\comprobante.html");
+//        }
+//        if (reportFormat.equalsIgnoreCase("pdf")){
+//
+//        }
+//
+//        return "";
+//    }
 }
