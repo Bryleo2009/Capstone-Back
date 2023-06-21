0e0e348e94154d623978932be69f0b4f872314c3
CarlosS123A <salinasreyesc2002@gmail.com>
2023-06-21 04:06:30 -0500

    AvanceCRUDSEMIOK

--- src/main/java/com/ofsystem/Service/Imple/Cliente/ListaDeseoServiceImpl.java
+++ src/main/java/com/ofsystem/Service/Imple/Cliente/ListaDeseoServiceImpl.java
@@ -4,11 +4,11 @@ import com.ofsystem.Mapper.Filter.SeguimientoListadeseosFilter;
 import com.ofsystem.Mapper.Filter.SeguimientoPedidoFilter;
 import com.ofsystem.Mapper.IListadeseoMapper;
 import com.ofsystem.Mapper.IPedidoMapper;
-import com.ofsystem.Model.Cliente.ListaDeseos;
+
 import com.ofsystem.Model.Cliente.Pedido;
-import com.ofsystem.Repo.Cliente.IListaDeseoRepo;
+
 import com.ofsystem.Repo.Cliente.IPedidoRepo;
-import com.ofsystem.Service.Service.Cliente.IListaDeseoService;
+
 import com.ofsystem.Service.Imple.CRUDServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
